55a70d250321656dad5b487cefd6ff15adede0b7
Loopy_BAEK <likipop@naver.com>
2023-06-04 20:49:26 +0900

    스프링 부트 3 시작하기

--- src/main/java/me/rose/springbootdeveloper/SpringBootDeveloperApplication.java
+++ src/main/java/me/rose/springbootdeveloper/SpringBootDeveloperApplication.java
@@ -3,9 +3,26 @@ package me.rose.springbootdeveloper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication  // 스프링 부트 사용에 필요한 기본 설정을 해준다.
 public class SpringBootDeveloperApplication {
     public static void main(String[] args) {
+        // SpringApplication.run() 메소드는 애플리케이션을 실행한다.
+        // 첫 번째 인수는 스프링 부트 3 애플리케이션의 메인 클래스로 사용할 클래스
+        // 두 번째 인수는 커맨드 라인의 인수들을 전달한다.
         SpringApplication.run(SpringBootDeveloperApplication.class, args);
     }
+
+    // @SpringBootConfiguration
+    // 스프링 부트 관련 설정을 나타내는 어노테이션이다.
+    // @Configuration을 상속해서 만든 어노테이션이다.
+
+    // @ComponentScan
+    // 사용자가 등록한 빈을 읽고 등록하는 어노테이션이다.
+    // 이 어노테이션은 @Component라는 어노테이션을 가진 클래스들을 찾아 빈으로 등록하는 역할을 한다.
+    // 그렇다고 모든 빈에 @Component만 사용하는 게 아니다.
+
+    // @EnableAutoConfiguration
+    // 스프링 부트에서 자동 구성을 활성화하는 어노테이션이다.
+    // 이 어노테이션은 스프링 부트 서버가 실행될 때 스프링 부트의 메타 파일을 읽고 정의된 설정들을 자동으로 구성하는 역할을 수행한다.
+    // spring.factories 파일에 클래스들이 모두 @EnableAutoConfiguration을 사용할 때 자동 설정된다.
 }
---
+++ src/main/java/me/rose/springbootdeveloper/TestController.java
@@ -0,0 +1,19 @@
+package me.rose.springbootdeveloper;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public String test(){
+        return "Hello, world!";
+    }
+}
+
+// @RestController : 라우터 역할을 하는 어노테이션
+// 라우터란 HTTP 요청과 메소드를 연결하는 장치
+// 이 어노테이션이 있어야 클라이언트의 요청에 맞는 메소드를 실행할 수 있다.
+// @Controller 어노테이션에 @ResponseBody 어노테이션이 합쳐진 결과물이다.
+// @Controller 어노테이션이 @ComponentScan을 통해 빈으로 등록되는 이유는 @Controller 어노테이션에서 @Component 어노테이션을 가지고 있기 때문이다.
+// @Configuration, @Repository, @Service 어노테이션도 모두 @Component 어노테이션을 가지고 있지만 빈이 무슨 역할을 하는지 명확히 구분하기 위해 다른 이름으로 덮어둔 것이다.
